c6fa72a671525bdb73861516edcf5a3f850faf5b
ackemmanuelkaruri <ackemmanuelchurchkaruri@gmail.com>
2025-10-26 00:34:32 +0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,5 +1,6 @@
+// Service Worker Registration - Fixed path for Render deployment
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/anglicankenya/service-worker.js')
+  navigator.serviceWorker.register('/service-worker.js')
     .then(reg => console.log('Service Worker registered:', reg))
     .catch(err => console.error('Service Worker registration failed:', err));
 }
