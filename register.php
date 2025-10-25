6b2d44c05219382694e54fd9318765073c686404
ackemmanuelkaruri <ackemmanuelchurchkaruri@gmail.com>
2025-10-26 01:21:37 +0300

    Update register.php

--- register.php
+++ register.php
@@ -1,4 +1,9 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/php_errors.log');
 /**
  * ============================================
  * MULTI-TENANT REGISTRATION SYSTEM
